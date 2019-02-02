3744e8797c3427087d2d76048592d37405cc9970
Juan Diego Alonso <tontolanona@gmail.com>
2019-02-02 23:26:08 +0100

    Ampliación del juego y corrección de errores de traducción

--- PiedraPapelTijera/src/MoveWords.java
+++ PiedraPapelTijera/src/MoveWords.java
@@ -6,8 +6,8 @@ public class MoveWords{
     public static final int GANA = 1;
     public static final int PIERDE = 2;
 
-    private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA"};
-    private static final String[] validCommands = {"SALIR", "HELP"};
+    private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA", "LAGARTIJA","SPOCK"};
+    private static final String[] validCommands = {"SALIR", "AYUDA"};
 
     private Random rnd;
     
@@ -67,7 +67,7 @@ public class MoveWords{
 
 	    if (first_i == second_i) return EMPATE;
 	    
-	    return (( (first_i +1) % validMoves.length ) == second_i ) ? GANA: PIERDE;
+	    return ((( (first_i +1) % validMoves.length ) == second_i ) || ( (first_i +3) % validMoves.length ) == second_i)  ? GANA: PIERDE;
 	}
 	
 } 
